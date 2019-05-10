24b652cebde0bf4b6e377462c8c5277c0df1ed57
Carambis <lepeshko1997@mail.ru>
2019-05-10 22:59:17 +0300

    add upload form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from '../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  constructor(private http: HttpClient) {
+  }
+
   title = 'checkMathClient';
+  uploadedFiles: any[] = [];
+  url: string = environment.baseUrl + 'recognition/uploadFile';
+
+  static downloadFile(data: Response) {
+    const blob = new Blob([JSON.parse(data.toString())], {type: 'text/csv'});
+    const url = window.URL.createObjectURL(blob);
+    window.open(url);
+  }
+
+  onUpload(event) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+  }
+
+  upload(event) {
+    this.http.post(this.url, event.files[0]).subscribe((data: Response) => AppComponent.downloadFile(data));
+  }
 }
